78878ed87b66e2dd9a2df797d85e6377c867781c
ion cionca <ion.cionca@epfl.ch>
2017-05-24 14:08:11 +0200

    nouvelle version ticketshop avec no dossiers automatiques

---
+++ htdocs/extra/js/cff.js
@@ -0,0 +1,50 @@
+var crtlayer = 1;
+
+function ShowLayer(layerid) {
+    var crtlayername= "Layer"+crtlayer;
+    var layername   = "Layer"+layerid;
+    document.getElementById(crtlayername).style.visibility='hidden';
+    document.getElementById(layername).style.visibility='visible';
+    crtlayer = layerid;
+}
+
+function showHelp (helpURL) 
+{
+  window.open(helpURL,'helpwindow','width=330,height=800,menubar=no,scrollbars=yes');
+}
+
+function switchOptions (id) {
+
+	var selector   = document.getElementById(id);
+	var item	   = selector.options[selector.selectedIndex].value;
+	if (item == '') {
+		document.getElementById('choix_cf').style.display		= 'block';
+		document.getElementById('choix_dossier').style.display	= 'none';
+	} else {
+		document.getElementById('choix_cf').style.display		= 'none';
+		document.getElementById('choix_dossier').style.display	= 'block';
+	}
+}
+
+function toggleDiv (divid) {
+	var mydiv = document.getElementById(divid); 
+	if (mydiv.style.display == 'none') {
+		mydiv.style.display = 'block';
+	} else {
+		mydiv.style.display = 'none';
+	}
+} 
+
+//	-
+
+function loadHTML (div_id, url) {
+	var obj = new XMLHttpRequest();
+	obj.open("GET", url);
+	obj.send();
+	obj.onreadystatechange=function() 
+	{
+	  if(obj.readyState==4) 
+		document.getElementById(div_id).innerHTML = obj.responseText; 
+		document.getElementById(div_id).style.display = 'block'; 
+	}
+}
