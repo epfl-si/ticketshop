92ee95cb4f8e67544cdab2a1aeee44966b5ff2b0
Azecko <samikarin24@gmail.com>
2025-02-11 15:06:09 +0100

    [feature] list DFs
    Easy function to list the DFs on the page as for the funds.

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const [funds, setFunds] = useState([]);
+  const [dfs, setDfs] = useState([]);
   const { data: session, status } = useSession();
 
   type fund = {
@@ -24,11 +25,31 @@ export default function Home() {
     workflowid: number,
   }
 
+  type df = {
+    requestID: number,
+    sciper: number,
+    name: string,
+    dates: string,
+    destination: string,
+    concatFunds: number,
+    imputation: {
+      fund: number,
+      cf: string,
+    }
+  }
+
   useEffect(() => {
     fetch(`/api/funds/${session?.user.sciper}`)
       .then(res => res.json())
       .then(data => {
         setFunds(data);
+      })
+      .catch(err => console.log(err));
+
+    fetch(`/api/dfs/${session?.user.sciper}`)
+      .then(res => res.json())
+      .then(data => {
+        setDfs(data);
       })
       .catch(err => console.log(err));
   }, [session]);
@@ -46,12 +67,29 @@ export default function Home() {
       ))
     }
   }
+
+  function displayDfs(status: "authenticated" | "loading" | "unauthenticated", dfs: df[]) {
+    if(status !== "authenticated") {
+      return;
+    } else if(!dfs.length) {
+      return <h1 className="text-2xl font-bold text-red-500">Error: No DFS found</h1>;
+    } else {
+      return dfs.map((df:df, index:number) => (
+        <div key={index} className="flex flex-col items-center justify-center w-full h-20 px-4 py-2 m-2 bg-white border border-gray-200 rounded-md shadow-md sm:w-96">
+          <h1 className="text-lg font-bold text-red-500">{df.destination}</h1>
+        </div>
+      ))
+    }
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {status == "authenticated" ? <SignOutButton /> : <SignInButton btnValue="Sign In" redirectPath="/"/>}
       <div>
         {
           displayFunds(status, funds)
+        }
+        {
+          displayDfs(status, dfs)
         }
       </div>
     </div>
