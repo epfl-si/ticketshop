3d19f79ed4be7008fbca4d442aee88913f7b68de
Azecko <samikarin24@gmail.com>
2025-06-06 11:46:04 +0200

    [refactor] Displaying dfs and funds using `updateUser` and `getUser` routes

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     labelfr: string,
     name: string,
     persid: string,
-    resourceid: string,
+    resourceId: string,
     status: string,
     type: string,
     value: string,
@@ -39,19 +39,23 @@ export default function Home() {
   }
 
   useEffect(() => {
-    fetch(`/api/funds/${session?.user.sciper}`)
-      .then(res => res.json())
-      .then(data => {
-        setFunds(data);
-      })
-      .catch(err => console.log(err));
+    if(!session?.user.sciper) return;
 
-    fetch(`/api/dfs/${session?.user.sciper}`)
-      .then(res => res.json())
-      .then(data => {
-        setDfs(data);
-      })
-      .catch(err => console.log(err));
+    fetch(`/api/updateUser`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ sciper: parseInt(session?.user.sciper) }),
+    }).then(() => {
+      fetch(`/api/getUser/${session?.user.sciper}`)
+        .then(res => res.json())
+        .then(data => {
+          setFunds(data.funds);
+          setDfs(data.dfs);
+        })
+        .catch(err => console.log(err));
+    })
   }, [session?.user.sciper]);
 
   function displayFunds(status: "authenticated" | "loading" | "unauthenticated", funds: {error: string} | fund[]) {
@@ -61,10 +65,10 @@ export default function Home() {
       return <h1 className="text-lg">{funds.error}</h1>;
     } else {
       return funds.map((fund:fund) => (
-        <label className="inline-flex items-center cursor-pointer" key={fund.resourceid}>
+        <label className="inline-flex items-center cursor-pointer" key={fund.resourceId}>
           <input type="checkbox" value="" className="sr-only peer" />
           <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-red-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-red-600"></div>
-          <span className="ml-2 text-sm text-black">{fund.resourceid}</span>
+          <span className="ml-2 text-sm text-black">{fund.resourceId}</span>
         </label>
       ))
     }
