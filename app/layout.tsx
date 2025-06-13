f70ef1905bab3263cf39aa6d53ca92f763d34a70
Azecko <samikarin24@gmail.com>
2025-06-13 12:29:54 +0200

    [refactor] Removed useless fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 import { Header } from "./components/Header";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -31,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <SessionProvider session={session}>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body>
           <Header />
           {children}
         </body>
