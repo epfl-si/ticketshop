810c276e34629dd37fdd6ecc785f537857932265
Dwesh163 <dwesh163@protonmail.com>
2025-11-17 12:12:48 +0100

    [refactor] remove unnecessary datetime logging and streamline log function parameters

--- src/services/log.ts
+++ src/services/log.ts
@@ -1,95 +1,70 @@
 import { User } from "next-auth";
-import { getUser } from "./auth";
 
-async function web({
-	user, endpoint, ip, requestId, message
-}: { user?: User, endpoint?: string, ip?: string | null, requestId? : string, message?: string }): Promise<void>{
-	const datetime = new Date();
-	logs(
-		{
-			datetime,
-			type: "web",
-			user: user ?
-			{
-				id: user?.userId,
-				name: user?.name,
-				groups: user?.groups
-			} : undefined,
-			ip,
-			endpoint,
-			requestId,
-			message
-		});
+async function web({ user, endpoint, ip, requestId, message, method }: { user?: User, endpoint?: string, ip?: string | null, requestId?: string, message?: string, method?: string }): Promise<void> {
+	logs({
+		type: "web",
+		user: user ?
+			{ id: user?.userId, name: user?.name, groups: user?.groups } : undefined,
+		ip,
+		endpoint,
+		requestId,
+		message,
+		method,
+	});
 }
 
-async function api({
-	message, action, url, status, method, requestId
-}: { action: string, message: string, url: string, status?: number, method: string, ip?: string, requestId?: string }): Promise<void> {
-	const datetime = new Date();
-	logs(
-		{
-			datetime,
-			type: "api",
-			message,
-			action,
-			url,
-			status,
-			method,
-			requestId
-		});
+async function api({ message, action, url, status, method, requestId }: { action: string, message: string, url: string, status?: number, method: string, ip?: string, requestId?: string }): Promise<void> {
+	logs({
+		type: "api",
+		message,
+		action,
+		url,
+		status,
+		method,
+		requestId,
+	});
 }
 
-async function soap({
-	message, action, endpoint, status, method, requestId, direction, soap
-}: { action: string, message?: string, endpoint: string, status?: number, method: string, ip?: string | null, direction?: string, requestId? : string, soap?: string }): Promise<void>{
-	const datetime = new Date();
-	logs(
-		{
-
-			datetime,
-			type: "soap",
-			message,
-			action,
-			endpoint,
-			status,
-			method,
-			direction,
-			soap,
-			requestId
-		});
+async function soap({ message, action, endpoint, status, method, requestId, direction, soap }: { action: string, message?: string, endpoint: string, status?: number, method: string, ip?: string | null, direction?: string, requestId?: string, soap?: string }): Promise<void> {
+	logs({
+		type: "soap",
+		message,
+		action,
+		endpoint,
+		status,
+		method,
+		direction,
+		soap,
+		requestId,
+	});
 }
 
-async function database({
-	message, action, endpoint, status, method, requestId, itemId, itemType, value, fundId, travelId, direction
-}: { action: string, message?: string, endpoint?: string, status?: string, method?: string, direction?: string, requestId? : string | undefined, itemId?: string, itemType?: string, value?: boolean, fundId?: string, travelId?: string }): Promise<void>{
-	const datetime = new Date();
-	logs(
-		{
-			datetime,
-			type: "database",
-			message,
-			action,
-			endpoint,
-			status,
-			method,
-			itemId,
-			itemType,
-			requestId,
-			value,
-			fundId,
-			travelId,
-			direction
-		});
+async function database({ message, action, endpoint, status, method, requestId, itemId, itemType, value, fundId, travelId, direction }: { action: string, message?: string, endpoint?: string, status?: string, method?: string, direction?: string, requestId?: string | undefined, itemId?: string, itemType?: string, value?: boolean, fundId?: string, travelId?: string }): Promise<void> {
+	logs({
+		type: "database",
+		message,
+		action,
+		endpoint,
+		status,
+		method,
+		itemId,
+		itemType,
+		requestId,
+		value,
+		fundId,
+		travelId,
+		direction,
+	});
 }
 
-function logs(param: object) {
+function logs(params: object) {
 	const paramWithoutUndefined = Object.fromEntries(
-		Object.entries(param).filter(([key, value]) => value != null && value != undefined)
+		Object.entries(params).filter(([, value]) => value !== null),
 	);
 
-	console.log();
-	console.log(paramWithoutUndefined);
-	console.log();
+	console.info(JSON.stringify(paramWithoutUndefined));
 }
 
-export default { web, api, soap, database };
+const log = { web, api, soap, database };
+
+export default log;
