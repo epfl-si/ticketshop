d92193273db6d14af3478575571c719b49e4a08d
FlizzerMDX <pa64wln@eduvaud.ch>
2025-11-25 16:30:46 +0100

    [fix] add syncUserData for artefactServer

--- src/app/api/artifactServer/route.ts
+++ src/app/api/artifactServer/route.ts
@@ -1,6 +1,7 @@
 import { parseArtifactRequest, generateArtifactIDResponse, generateArtifactResponse, generateSoapFault, createXmlResponse } from "@/lib/soap";
 import { processArtifactIDRequest, processArtifactRequest } from "@/services/artifact";
 import { ArtifactIDResponse, ArtifactResponse } from "@/types/artifact";
+import { syncUserData } from "@/lib/database";
 
 import log from "@/services/log";
 import { cookies } from "next/headers";
@@ -11,16 +12,17 @@ export async function POST(req: Request) {
 		const xmlData = await req.text();
 		const request = parseArtifactRequest(xmlData);
 
-
 		const cookieStore = await cookies();
 		cookieStore.set("requestId", requestId);
 
 		log.soap({ endpoint: "/api/artifactServer", action: "artifactServer", method: "POST", soap: xmlData, direction: "inband", ip: req.headers.get("x-forwarded-for"), requestId });
 
+		// XML SOAP response telling the CFF if the EPFL user has the railticket (accred) right.
 		if (request.email) {
 			const result = await processArtifactIDRequest(request.email, xmlData);
 
 			if (result.success && result.data) {
+				await syncUserData(result.data.artifactID);
 				const responseXML = generateArtifactIDResponse(result.data as ArtifactIDResponse);
 				log.soap({ endpoint: "/api/artifactServer", action: "artifactServer", method: "POST", soap: responseXML, direction: "outband", status: 200, ip: req.headers.get("x-forwarded-for"), requestId });
 				return createXmlResponse(responseXML, 200);
@@ -31,7 +33,9 @@ export async function POST(req: Request) {
 			}
 		}
 
+		// XML SOAP response with EPFL user's fund(s) to the CFF HTTP request.
 		if (request.artifactID) {
+			await syncUserData(request.artifactID);
 			const result = await processArtifactRequest(request.artifactID, xmlData);
 
 			if (result.success && result.data) {
