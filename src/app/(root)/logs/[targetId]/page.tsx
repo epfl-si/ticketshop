6ae46ae335d617b7b19345f1e0935845f137f562
FlizzerMDX <pa64wln@eduvaud.ch>
2026-06-10 11:14:44 +0200

    [feature] url format /logs/123456?t=fund_disabled, and basic monitoring
    close #39
    close #47
    close #51

---
+++ src/app/(root)/logs/[targetId]/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import LogsPageView from "@/views/log";
+import { useParams } from "next/navigation";
+
+export default function LogsPage() {
+	const params = useParams();
+	const targetID = params.targetId as string;
+	return <LogsPageView targetID={targetID} />;
+}
